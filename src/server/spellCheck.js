a3a874f3fd45b39706c74128302d7a3dedc801dc
Jessica Peck <jessypeck@gmail.com>
2018-06-04 16:46:13 -0700

    capitalizes start of string

--- src/server/spellCheck.js
+++ src/server/spellCheck.js
@@ -1,14 +1,17 @@
-// Apply correct indefinite (a/an) articles
+
 const spellCheck = (string) => {
     // Type checking, throws error if not string
     if (typeof string !== "string"){
         console.error('ERROR in spellCheck.js: Expected string. Recieved ' + typeof string +'.');
     }
-
+    // Applies correct indefinite (a/an) articles
     const regex = /(^|\s)([aA])(\s[aeiouAEIOU])/;
     if (string.match(regex)){
         string = string.replace(regex, '$1an$3');
     }
+
+    // Capitalizes first letter of string. Might later need upgrading for every sentence?
+    string = string.charAt(0).toUpperCase() + string.slice(1);
     return string;
 }
 
