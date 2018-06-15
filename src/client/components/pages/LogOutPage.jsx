5fe963a048ec910cf43bd4c29a73990ee77d1fe1
Jessica Peck <jessypeck@gmail.com>
2018-06-14 19:26:09 -0700

    Styled LogOutPage & added buttons

--- src/client/components/pages/LogOutPage.jsx
+++ src/client/components/pages/LogOutPage.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import NavBar from './../NavBar.jsx';
 import { setLoggedOut } from '../../actions/authActions';
 import {connect} from 'react-redux';
+import IconButton from './../IconButton.jsx';
+import faHome from '@fortawesome/fontawesome-pro-light/faHome';
+import faBook from '@fortawesome/fontawesome-pro-light/faBookOpen';
+import faLogin from '@fortawesome/fontawesome-pro-light/faSignInAlt';
 
 
 class LogOutPage extends React.Component{
@@ -30,9 +34,14 @@ class LogOutPage extends React.Component{
 
   render(){
     return(
-      <div>
-        <div className='pageTitle'>You've logged out!</div>
-        If this was a mistake, click here to log back in.
+      <div className='logOutPageWrapper'>
+        <div className='logOutTitle'>You are now logged out.</div>
+        <div className="logOutText">Thanks for visiting!</div>
+        <div className="logOutPageButtons">
+            <IconButton label='Home' url='/' icon={faHome} />
+            <IconButton label='Browse' url='ruins' icon={faBook} />
+            <IconButton label='Login' url='login' icon={faLogin} />
+        </div>  
       </div>
     )
   }
