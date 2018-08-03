882a6b647703b542c11f28cff745aa6fbf3447df
Jessica Peck <jessypeck@gmail.com>
2018-08-03 13:45:17 -0700

    Changed classes to be modifiable based on props

--- src/client/components/SubmitButton.jsx
+++ src/client/components/SubmitButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SubmitButton = (props) => (
     <button 
         //type='submit' 
-        className='button button--login ripple' 
+        className={'button ripple ' + props.className}
         //value={props.label}
     >
         {props.label}
