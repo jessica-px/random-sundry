8a9a6552300f25b43c38ed34ea00bee4b1d0ecc3
Jessica Peck <jessypeck@gmail.com>
2018-11-09 09:45:09 -0800

    Added error catching to index.html file serving

--- src/server/server.js
+++ src/server/server.js
@@ -226,10 +226,13 @@ app.post('/auth/change-email', function(req, res, next) {
 
 // Direct all other routes to index.html
 
-app.get('*', function (req, res){
-    console.log('Loading * route. User is: ' + req.user.local.username);
-    console.log(req.isAuthenticated())
-    res.sendFile(path.resolve(__dirname +'/../..', 'public', 'index.html'))
+app.use(express.static('public')) 
+app.get('*', (req, res)=>{
+  res.sendFile(path.resolve(__dirname +'/../..', 'public', 'index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
 })
 
 
