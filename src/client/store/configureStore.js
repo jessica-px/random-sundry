233873c8f95c30abf262595d8e9e4277006a5da4
Jessica Peck <jessypeck@gmail.com>
2018-11-12 12:01:55 -0800

    Fixed incorrect createStore() arguments

--- src/client/store/configureStore.js
+++ src/client/store/configureStore.js
@@ -1,14 +1,16 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import authReducer from '../reducers/authReducer';
 import thunk from 'redux-thunk';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
   const store = createStore(
     combineReducers({
       auth: authReducer
     }), 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk),
+    composeEnhancers(
+      applyMiddleware(thunk),
+    )
   );
   
   return store;
