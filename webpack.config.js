ccc52d6d75e21186fe15d1541350808b9d2427b1
Jessica Peck <jessypeck@gmail.com>
2018-11-14 15:57:43 -0800

    Added url-loader for .svg files

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,9 @@ const config = {
   },
   module: {
     rules: [
+      { 
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+        loader: 'url-loader?limit=100000' },
       {
         test: /(\.css|.scss)$/,
         use: [{
